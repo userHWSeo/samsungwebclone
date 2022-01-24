00ec0f224f281ad466fab67553dc964cfd66bf13
userHWSeo <gmldnjseocnd@naver.com>
2022-01-25 00:51:02 +0900

    0124 update
    phone.html and slider update

---
+++ js/phone_img.js
@@ -0,0 +1,19 @@
+const containerSlide = document.querySelector('.container_slide');
+const phoneImgs = document.querySelector('.container_slide li');
+let currentIdx = 0;
+//Btn
+const prevBtn = document.querySelector('.left-btn');
+const nextBtn = document.querySelector('.right-btn');
+
+function moveSlide(num){
+    containerSlide.style.left = -num * 700 + 'px';
+    currentIdx = num;
+}
+
+//listener
+prevBtn.addEventListener('click', function(){
+    if(currentIdx !== 0) moveSlide(currentIdx - 1);
+})
+nextBtn.addEventListener('click', function(){
+    if(currentIdx !== 7) moveSlide(currentIdx + 1);
+})
