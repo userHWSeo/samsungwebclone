c2729f2944020b2a8c5e0c5b8a7c9127d08ef970
userHWSeo <gmldnjseocnd@naver.com>
2022-01-28 00:36:29 +0900

    0128 update
    supplement

--- js/phone/phone_img_slide.js
+++ js/phone/phone_img_slide.js
@@ -1,6 +1,6 @@
 const containerSlide = document.querySelector('.container_slide');
 const phoneImgs = document.querySelector('.container_slide li');
-let currentIdx = 0;
+let currentIdx = 0; // 현재 이미지
 
 //Btn
 const prevBtn = document.querySelector('.left-btn');
@@ -11,10 +11,16 @@ function moveSlide(num){
     currentIdx = num;
 }
 
+
 //listener
 prevBtn.addEventListener('click', function(){
-    if(currentIdx !== 0) moveSlide(currentIdx - 1);
+    if(currentIdx !== 0) {
+        moveSlide(currentIdx - 1);
+    }
 })
+
 nextBtn.addEventListener('click', function(){
-    if(currentIdx !== 7) moveSlide(currentIdx + 1);
+    if(currentIdx !== 8 - 1) {  // 현재 이미지 !== 총 이미지 - 1
+    moveSlide(currentIdx + 1);
+    }
 })
